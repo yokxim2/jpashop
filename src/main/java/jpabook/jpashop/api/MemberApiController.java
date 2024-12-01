6826cb7bd05598548366a891f52d65c3de0adaac
yokxim2 <yooksi53@gmail.com>
2024-12-02 00:19:46 +0900

    feat: API 호출 > 유저 저장 API v2 생성

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.api;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,21 @@ public class MemberApiController {
 	public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
 		Long id = memberService.join(member);
 		return new CreateMemberResponse(id);
+	}
+
+	@PostMapping("/api/v2/members")
+	public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+		Member member = new Member();
+		member.setName(request.getName());
+
+		Long id = memberService.join(member);
+		return new CreateMemberResponse(id);
+	}
+
+	@Data
+	static class CreateMemberRequest {
+		@NotEmpty
+		private String name;
 	}
 
 	@Data
