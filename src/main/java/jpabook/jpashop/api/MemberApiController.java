216dd298f81c1de38af32bdac84a5abc1fb5fc66
yokxim2 <yooksi53@gmail.com>
2024-12-02 00:14:38 +0900

    feat: API 호출 > 유저 저장 API v1 생성

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.api;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+	private final MemberService memberService;
+
+	@PostMapping("/api/v1/members")
+	public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+		Long id = memberService.join(member);
+		return new CreateMemberResponse(id);
+	}
+
+	@Data
+	static class CreateMemberResponse {
+		private Long id;
+
+		public CreateMemberResponse(Long id) {
+			this.id = id;
+		}
+	}
+}
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,12 +1,20 @@
 package jpabook.jpashop.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter @Setter
@@ -16,6 +24,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
